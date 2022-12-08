562e97025c6f1c97440a240fcc87a0715f091081
Gokul Rangarajan <gokulraja2006@gmail.com>
2022-12-08 23:51:36 +0530

    Activemq exeternalized parameter

--- activemq/README.md
+++ activemq/README.md
@@ -1,6 +1,28 @@
 ## Active MQ Image
 Apache ActiveMQ 5.17.2
 
+### Configuration
+Set the following options in environment variable to:
+
+| Environment Variable | Default | Description |
+|----------------------|---------|-------------|
+| Q_WEBCONSOLE_BIND_ALL| true    |true to use 0.0.0.0; false to use 127.0.0.1|
+| Q_WEBCONSOLE_ADMIN_USERNAME| admin    |admin username for webconsole|
+| Q_WEBCONSOLE_ADMIN_PASSWORD| admin    |admin password for webconsole|
+| Q_USERNAME| system    |User name for MQ Security|
+| Q_PASSWORD| manager    |password for MQ Security|
+
+Example:
+```
+docker run -it \
+-p 61616:61616 -p 8161:8161 \
+-e Q_WEBCONSOLE_BIND_ALL=true -e Q_WEBCONSOLE_USERNAME=user -e Q_WEBCONSOLE_PASSWORD=user \
+-e Q_WEBCONSOLE_ADMIN_USERNAME=admin -e Q_WEBCONSOLE_ADMIN_PASSWORD=adminpwd \
+gokul2787/devtools:activemq
+```
+
+<hr />
+
 Based on gokul2787/devtools:openjdk17
 
 openjdk 17.0.2 2022-01-18
--- activemq/activemq-entrypoint.sh
+++ activemq/activemq-entrypoint.sh
@@ -1,5 +1,38 @@
 #!/bin/sh
 
+modified_user_name="admin"
+modified_password="admin"
+
+echo "Setting configurations"
+
+if [ "$Q_WEBCONSOLE_BIND_ALL" = "false" ]; then
+    echo "Not binding to 0.0.0.0. Console maynot be exposed out"
+else
+    sed -i 's#<property name="host" value="127.0.0.1"/>#<property name="host" value="0.0.0.0"/>#' /opt/activemq/apache-activemq-5.17.3/conf/jetty.xml
+fi
+
+if [ -n "$Q_USERNAME" ]; then
+    echo "Setting username"
+    sed -i "s#activemq.username=system#activemq.username=$Q_WEBCONSOLE_USERNAME#" /opt/activemq/apache-activemq-5.17.3/conf/credentials.properties
+fi
+
+if [ -n "$Q_PASSWORD" ]; then
+    echo "Setting password"
+    sed -i "s#activemq.password=manager#activemq.password=$Q_WEBCONSOLE_PASSWORD#" /opt/activemq/apache-activemq-5.17.3/conf/credentials.properties
+fi
+
+if [ -n "$Q_WEBCONSOLE_ADMIN_USERNAME" ]; then
+    echo "Setting web console admin user"
+    modified_user_name=$Q_WEBCONSOLE_ADMIN_USERNAME
+fi
+
+if [ -n "$Q_WEBCONSOLE_ADMIN_PASSWORD" ]; then
+    echo "Setting web console admin user"
+    modified_password=$Q_WEBCONSOLE_ADMIN_PASSWORD
+fi
+
+sed -i "s#admin: admin, admin#$modified_user_name: $modified_password, admin#" /opt/activemq/apache-activemq-5.17.3/conf/jetty-realm.properties
+
 echo "Starting active mq"
 cd $ACTIVEMQ_HOME/bin
 ./activemq console
